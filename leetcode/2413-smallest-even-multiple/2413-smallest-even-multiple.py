92cac5c7260028d02caf1a6306d291aa781f2102
Tesfamichael Tafere <tesfamichael132@gmail.com>
2026-03-01 23:01:18 +0300

    Time: 0 ms (100.00%), Space: 19.2 MB (77.07%) - A2SV Companion

---
+++ leetcode/2413-smallest-even-multiple/2413-smallest-even-multiple.py
@@ -0,0 +1,15 @@
+// Problem: Smallest Even Multiple
+// Difficulty: Easy
+// URL: https://leetcode.com/problems/smallest-even-multiple/
+// Language: Python3
+// Submitted: 2026-03-01T20:01:03.000Z
+// Pushed via A2SV Companion
+
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        if n % 2 == 0:
+            return n
+        else:
+            return 2*n
+
+        # updated
