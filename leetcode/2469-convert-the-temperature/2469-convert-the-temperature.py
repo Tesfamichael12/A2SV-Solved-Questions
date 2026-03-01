0ec560c8af3ab5aae248069e024d6125d6b6eac7
Tesfamichael Tafere <tesfamichael132@gmail.com>
2026-03-01 12:11:23 +0300

    Time: 0 ms (100.00%), Space: 19.3 MB (87.32%) - A2SV Hub

--- leetcode/2469-convert-the-temperature/2469-convert-the-temperature.py
+++ leetcode/2469-convert-the-temperature/2469-convert-the-temperature.py
@@ -1,3 +1,10 @@
+// Problem: Convert the Temperature
+// Difficulty: Easy
+// URL: https://leetcode.com/problems/convert-the-temperature/
+// Language: Python3
+// Submitted: 2026-03-01T09:10:01.000Z
+// Pushed via A2SV Hub
+
 class Solution:
     def convertTemperature(self, celsius: float) -> List[float]:
         return [celsius + 273.15, celsius*1.80 + 32.00]
